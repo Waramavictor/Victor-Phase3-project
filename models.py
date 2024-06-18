5929c2df0207a2d069e095c0878c9f9b87d303cc
waramavictor <victorkinyua16@gmail.com>
2024-06-18 09:41:57 +0300

    project complete

--- models.py
+++ models.py
@@ -1,31 +1,4 @@
 
-# import datetime
-# from sqlalchemy import create_engine, Column, Integer, String, DateTime
-# from sqlalchemy.ext.declarative import declarative_base
-
-# Base = declarative_base()
-
-# class Appointment(Base):
-#     __tablename__ = 'appointments'
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String)
-#     date_time = Column(DateTime)
-
-#     def __repr__(self):
-#         return f"<Appointment(name='{self.name}', date_time='{self.date_time}')>"
-
-# class Vote(Base):
-#     __tablename__ = 'votes'
-#     id = Column(Integer, primary_key=True)
-#     appointment_id = Column(Integer)
-#     vote = Column(Integer)  # 1 for yes, 0 for no
-
-#     def __repr__(self):
-#         return f"<Vote(appointment_id={self.appointment_id}, vote={self.vote})>"
-
-
-
-
 from sqlalchemy import Boolean
 import datetime
 from sqlalchemy import create_engine, Column, Integer, String, DateTime
